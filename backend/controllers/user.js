805d58be3f757fd25038e42ab5c6335f8d3b05ea
abhilashGayen <abhilashgayen@gmail.com>
2021-06-27 12:43:59 +0530

    Cleanup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.createUser = (req, res, next) => {
-  console.log(req.body);
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
       username: req.body.username,
